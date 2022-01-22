247cb0fb3bba71dab77f124b87953f00c2667a01
Radhakant Tiwari <therktiwadi@gmail.com>
2022-01-22 20:26:58 +0530

    basic structure 2

--- src/components/HomepageLeft.js
+++ src/components/HomepageLeft.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Navbar(props) {
   return (
-    <div className="col-md-6 bg-warning">
+    <div className="col-md-6" style={{ color: "#5861AE" }}>
       <h2>left part with graphics and register button</h2>
     </div>
   );
