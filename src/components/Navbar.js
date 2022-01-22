2bd178e9b7db5567261e95396d7145aa57e65762
Radhakant Tiwari <therktiwadi@gmail.com>
2022-01-22 17:48:44 +0530

    front page structure

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar(props) {
+  return (
+    <div className="col-xs-12 bg-primary">
+      <h2>navabar here</h2>
+    </div>
+  );
+}
+
+export default Navbar;
