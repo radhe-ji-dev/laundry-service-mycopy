033729fe75dc84835cff81877b8ac6f2d6b5acd2
Radhakant Tiwari <therktiwadi@gmail.com>
2022-01-23 15:25:35 +0530

    register page c1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Home from "./components/Homepage";
+import Register from "./components/Register";
 
 function App() {
   return (
     <>
-      <Home />
+      {/*<Home */}
+      <Register />
     </>
   );
 }
