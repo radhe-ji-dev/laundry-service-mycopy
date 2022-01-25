ba5985fae266d07037e0c2cd1e608c888294b608
Radhakant Tiwari <therktiwadi@gmail.com>
2022-01-25 14:31:07 +0530

    c4

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ function App() {
   return (
     <>
       {/*<Home />*/}
-      {/*<Register/>*/}
+      <Register />
       {/*<Dropdown />*/}
-      <Orderpage />
+      {/*<Orderpage />*/}
     </>
   );
 }
