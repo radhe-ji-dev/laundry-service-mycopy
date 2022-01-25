132adba10957bccfdcd4c86f34f3bff4f9095d75
Radhakant Tiwari <therktiwadi@gmail.com>
2022-01-25 12:45:25 +0530

    c4

--- src/components/Referbanner.js
+++ src/components/Referbanner.js
@@ -2,18 +2,8 @@ import React from "react";
 
 function Referbanner(props) {
   return (
-<<<<<<< HEAD
-    <div
-      className="col-s-12 border border-primary border-top-1 border-end-0 border-bottom-0 m-1"
-      style={{ backgroundColor: "var(--lightblue)" }}
-    >
-      <h5 className="offset-md-4">
-        Now Reffer and Earn 500 for every Refferal*
-      </h5>
-=======
     <div className="col-xs-12" style={{ backgroundColor: "var(--lightblue)" }}>
       <h5 className="refer">Now refer & earn ₹500 for every referal*</h5>
->>>>>>> 7ab39ef277b5e6e532f521beb2be4201fb545db6
     </div>
   );
 }
